1a1fbcca789989432a36b74efbfbd56569fb801d
safwan zullash <wi21b030@technikum-wien.at>
2022-05-18 14:42:37 +0200

    Initial commit

---
+++ src/main/java/com/example/maintenancemonitor1/MaintenanceMonitor1Application.java
@@ -0,0 +1,14 @@
+package com.example.maintenancemonitor1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaintenanceMonitor1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MaintenanceMonitor1Application.class, args);
+    }
+
+
+}
---
+++ src/test/java/com/example/maintenancemonitor1/MaintenanceMonitor1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.maintenancemonitor1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaintenanceMonitor1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
